c948cdd2f0b1410f390db6ebd9cf4c9240d4e651
sich97 <sich97@gmail.com>
2020-06-18 16:11:19 +0200

    Initial template

---
+++ src/no/chri/themist/Themist.java
@@ -0,0 +1,15 @@
+package no.chri.themist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Themist extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
